262156f0623b769255a1a1d645e99a5938e8531a
Kikuchi Motoki <kikuchi.motoki@outlook.com>
2018-12-25 22:26:32 +0900

    Add protobuf sample

--- device-capture/udp-sample/udp_client.cc
+++ device-capture/udp-sample/udp_client.cc
@@ -1,3 +1,5 @@
+#include "udp-sample/sample.pb.h"
+
 #include <boost/asio.hpp>
 #include <boost/system/error_code.hpp>
 
@@ -15,10 +17,10 @@ public:
           socket_(io_context, udp::endpoint(udp::v4(), 0)) {
   }
 
-  void send(std::string &data) {
+  void send(const std::string &data) {
     socket_.send_to(boost::asio::buffer(data), target_endpoint_);
   }
-  enum { max_length = 1024 };
+  // enum { max_length = 1024 };
 
 private:
   boost::asio::ip::address address_;
@@ -28,14 +30,20 @@ private:
 
 int main(int argc, char** argv) {
   std::string in;
-  std::getline(std::cin, in);
+  int32_t i = 0;
 
   try {
-      boost::asio::io_context io_context;
-      UdpClient client(io_context);
-      client.send(in);
+    boost::asio::io_context io_context;
+    UdpClient client(io_context);
+    while (true) {
+      std::getline(std::cin, in);
+      sample::Greeting g;
+      g.set_id(i++);
+      g.set_greetingmessage(in);
+      client.send(g.SerializeAsString());
+    }
   } catch (std::exception &e) {
-      std::cout << "exception " << e.what() << std::endl;
+    std::cout << "exception " << e.what() << std::endl;
   }
   return 0;
 }
--- device-capture/udp-sample/udp_server.cc
+++ device-capture/udp-sample/udp_server.cc
@@ -1,3 +1,5 @@
+#include "udp-sample/sample.pb.h"
+
 #include <boost/asio.hpp>
 #include <boost/system/error_code.hpp>
 
@@ -18,7 +20,8 @@ private:
         boost::asio::buffer(data_, max_length), sender_endpoint_,
         [&](boost::system::error_code ec, std::size_t bytes) {
           if (!ec) {
-            std::cout << "recived " << bytes << std::endl;
+            msg.ParseFromArray(&data_, bytes);
+            std::cout << "recived (" << bytes << "): " << msg.id() << " " << msg.greetingmessage() << std::endl;
           } else {
             std::cout << "error " << ec.value() << std::endl;
           }
@@ -30,6 +33,7 @@ private:
   udp::endpoint sender_endpoint_;
   enum { max_length = 1024 };
   char data_[max_length];
+  sample::Greeting msg;
 };
 
 int main(int argc, char** argv) {
